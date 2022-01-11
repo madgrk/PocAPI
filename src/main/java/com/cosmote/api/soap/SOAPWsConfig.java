62aee5073070c380b8652207d438483cba8bf566
Angelis Ioannis <iaggelis@gmail.com>
2022-01-11 10:24:38 +0200

    building

--- src/main/java/com/cosmote/api/soap/SOAPWsConfig.java
+++ src/main/java/com/cosmote/api/soap/SOAPWsConfig.java
@@ -9,8 +9,6 @@ import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
-import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
-import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
